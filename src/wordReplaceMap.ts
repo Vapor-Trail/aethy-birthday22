f542ebb0f712a97372fbc619fbe457778ed1059a
SputNikPlop <100245448+SputNikPlop@users.noreply.github.com>
2022-10-19 19:04:32 -0700

    Update wordReplaceMap.ts

--- src/wordReplaceMap.ts
+++ src/wordReplaceMap.ts
@@ -1,32 +1,32 @@
 import { html, TemplateResult } from "lit";
 
 export const wordReplaceMap: Record<string, string> = {
-  nidali1Clown:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_4adcf9a1f7ef43afb59243f26fde7845/default/dark/2.0",
-  nidali1Slam:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_3198daa9971143cbbc0bdbc44d8d87a0/default/dark/2.0",
-  nidali1Rage:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_b5c7966f57434590b767045a150975dc/default/dark/2.0",
-  nidali1Yes:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_a9dc49e835ae4b1381bfa4ed520b7882/default/dark/2.0",
-  nidali1Knife:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_2aa813508979418492e8d778ccd8d3df/default/dark/2.0",
-  nidali1Pew:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_b9227add596045da95066c117d899695/default/dark/2.0",
-  nidali1Pog:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_7a256807916c4f609de80dc7d71bc8dd/default/dark/2.0",
-  nidali1Thankyou:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_956e65ab20d048dc97c99ff52e296200/default/dark/2.0",
-  nidali1NidaCry:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_ecd71f3ad1c349459c0e64cd3e63eabd/default/dark/2.0",
+  aethia1Blankie:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_b7c1a562144b44509209cdc165ef0ead/default/dark/2.0",
+  aethia1Sing:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_396d0de91da44ac7b4e3cdf1bb9448e4/default/dark/2.0",
+  aethia1Pats:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_0c32e2de605a4a66a136a3bd21042ab9/default/dark/2.0",
+  aethia1Like:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_e2ac7e95803947acabdce70b2719aea5/default/dark/2.0",
+  aethia1Flower:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_38eac4b2a2af462da52781892f999eb0/default/dark/2.0",
+  aethia1Hype:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_00e71be9b7b14b4c912a0a86366f916d/default/dark/2.0",
+  aethia1Laugh:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_e502138c48cd48409f583737fa2f5e9d/default/dark/2.0",
+  aethia1Wow:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_9f8f7910f2e84ababd58275ccd6f5eb3/default/dark/2.0",
+  aethia1Hug:
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_111fd751e2ee44729bf73e302e0a00b6/default/dark/2.0",
   nidali1NidaHeart:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_bcc47849ebd94260b11402758d239de9/default/dark/2.0",
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_ba124aeeb3c24bcda15c1b3572aba694/default/dark/2.0",
   nidali1NidaWave:
     "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_5c9c5b1b75434261b39c615ad3b62dba/default/dark/2.0",
   nidali1PowderPat:
     "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_a2e29618473840c88b58137ff8b5f9de/default/dark/2.0",
   nidali1NidaHype:
-    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_da86360a2caa405199c8e270013f3ecd/default/dark/2.0",
+    "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_3babc35d0b2048a691d79e5558dbce45/default/dark/2.0",
   nidali1Huh:
     "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_8125272c5aba40fc839d7b62adef22ca/default/dark/2.0",
   nidali1Sip:
@@ -35,7 +35,7 @@ export const wordReplaceMap: Record<string, string> = {
     "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_d8858cdff3d449518d523310776019d9/default/dark/2.0",
   nidali1Swog:
     "//static-cdn.jtvnw.net/emoticons/v2/emotesv2_643649db5cfe49e38344033b8fb6518f/default/dark/2.0",
-  yasBih: "https://cdn.betterttv.net/emote/6167425a054a252a431ef190/2x",
+  yasBih: "https://cdn.betterttv.net/emote/5fd810b7a926f43485ce71ad/2x",
 
   // exceptions
   nidaClown:
